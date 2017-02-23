d67a2deba132fd10e49ac91c2099617ba650bbf9
MartinRoth <marsianus@gmail.com>
2017-02-23 16:52:19 +0100

    Added first regression test

---
+++ tests/testthat/test-FitGevFlex.R
@@ -0,0 +1,24 @@
+context("FitGevFlex")
+
+## TODO: Rename context
+## TODO: Add more tests
+
+library(data.table)
+
+testData <- fread("./testData.txt")
+
+test_that("regression test - normal fit", {
+
+  start <- c(7.5, 2.3, 0)
+
+  fpar <- function(p, xpar) {
+    loc   <- p[1]
+    scale <- p[2]
+    shape <- p[3]
+    list(loc = loc, scale = scale, shape = shape)
+  }
+
+  tmp <- FitGevFlex(testData[, 2, with = FALSE], start = start, fpar = fpar)
+
+  expect_equal_to_reference(tmp, file = "./referenceOutput/normalFit.rds")
+})
