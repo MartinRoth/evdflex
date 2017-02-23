163da64bf5d2b8e4931efaa8e7004c215065a129
MartinRoth <marsianus@gmail.com>
2017-02-23 16:57:00 +0100

    Added test for range fit

--- tests/testthat/test-FitGevFlex.R
+++ tests/testthat/test-FitGevFlex.R
@@ -22,3 +22,21 @@ test_that("regression test - normal fit", {
 
   expect_equal_to_reference(tmp, file = "./referenceOutput/normalFit.rds")
 })
+
+test_that("regression test - range fit", {
+
+  start <- c(7.5, 2.3, 0)
+
+  fpar <- function(p, xpar) {
+    loc   <- p[1]
+    scale <- p[2]
+    shape <- p[3]
+    list(loc = loc, scale = scale, shape = shape)
+  }
+
+  tmp <- fgev.flex_range(testData[, 2, with = FALSE],
+                         testData[, 3, with = FALSE],
+                         start = start, fpar = fpar)
+
+  expect_equal_to_reference(tmp, file = "./referenceOutput/rangeFit.rds")
+})
